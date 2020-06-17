aada15b4611c8ce534ea7fd734d388492f8569a8
osmanonurkoc <osmanonurkoc@gmail.com>
2020-06-17 06:01:50 +0300

    add installation script

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo Dosyalar indiriliyor
+git clone https://github.com/osmanonurkoc/wps-office-turkish.git
+echo Kurulum için root olun
+sudo cp -rf ./wps-office-turkish/wps-office /opt/kingsoft
+echo Türkçe Dil kuruldu
+sudo rm -rf ./wps-office-turkish
+
